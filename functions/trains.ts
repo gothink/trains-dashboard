dcc10cfb51ddd531706b3f2f436139ceb64b50b3
Zachary Goth <zach@gothink.dev>
2024-02-08 18:55:39 -0500

    trying new caching strategy, added marker icons

--- functions/trains.ts
+++ functions/trains.ts
@@ -6,22 +6,34 @@ export const onRequestGet: PagesFunction = async (context) => {
   request.headers.set("Origin", new URL(VIA_URL).origin);
 
   const cacheKey = new Request(VIA_URL, request);
-  const cache = caches.default;
+  // const cache = caches.default;
 
-  let response = await cache.match(cacheKey);
+  let response = await fetch(request, {
+    cf: {
+      cacheTtl: 30,
+      cacheEverything: true,
+      cacheKey: cacheKey.url,
+    }
+  });
 
-  if (!response) {
-    console.log(`cache miss`);
-    response = await fetch(request);
+  // if (!response) {
+  //   console.log(`cache miss`);
+    // response = await fetch(request, {
+    //   cf: {
+    //     cacheTtl: 30,
+    //     cacheEverything: true,
+    //     cacheKey: cacheKey.url,
+    //   }
+    // });
 
-    response = new Response(response.body, response);
-    response.headers.set("Access-Control-Allow-Origin", context.request.url);
-    response.headers.append("Vary", "Origin");
-    response.headers.set("Cache-Control", "max-age=30");
-    response.headers.delete("ETag");
+    // response = new Response(response.body, response);
+    // response.headers.set("Access-Control-Allow-Origin", context.request.url);
+    // response.headers.append("Vary", "Origin");
+    // response.headers.set("Cache-Control", "max-age=30");
+    // response.headers.delete("ETag");
 
-    context.waitUntil(cache.put(cacheKey, response.clone()));
-  }
+    // context.waitUntil(cache.put(cacheKey, response.clone()));
+  // }
   // console.log(context.request.cf.latitude, context.request.cf.longitude);  
   return response;
 };
