bed1e599d77c5aa571c1115f0ca09391578a146c
Zachary Goth <zach@gothink.dev>
2024-02-07 20:17:08 -0500

    replaced OpenLayerMap with Leaflet

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
-import type BaseEvent from 'ol/events/Event';
-
-const initialized = ref(false);
-const initCoords = [-76.54, 44.26];
-const initZoom = 8;
-const initRotation = 0;
-
-const center = ref(initCoords);
-const zoom = ref(initZoom);
-const rotation = ref(initRotation);
-const resolution = ref(0);
-
-const groupByStatus = ref(true);
+import { computed, onMounted, ref, watch } from 'vue';
+import L from 'leaflet';
 
 interface TrainTimes {
   station: string;
@@ -31,6 +19,7 @@ interface TrainTimes {
     scheduled: string;
   };
 }
+
 interface TrainInfo {
   departed: boolean;
   arrived: boolean;
@@ -44,102 +33,183 @@ interface TrainInfo {
   direction?: number;
   poll?: string;
   pollMin?: number;
-  number?: string;
-  status?: 'arr' | 'dep' | 'sch';
 }
+
 interface TrainInfoObject {
   [index: string]: TrainInfo;
 }
 
-function resolutionChanged(event: BaseEvent) {
-  resolution.value = event.target.getResolution();
-  zoom.value = event.target.getZoom();
-}
-function centerChanged(event: BaseEvent) {
-  center.value = event.target.getCenter();
-}
-function rotationChanged(event: BaseEvent) {
-  rotation.value = event.target.getRotation();
-}
+const initialized = ref(false);
 
-const trainData = ref<TrainInfoObject>();
+const center = ref<[number, number]>([44.26, -76.54]);
+const zoom = ref(8);
+
+const mapElem = ref<HTMLElement>();
+const leafMap = ref<L.Map>();
+const markers = ref<Record<string, L.Marker>>({});
+const mapBounds = ref<[[number, number], [number, number]]>([[0,0], [0,0]]);
+
+const groupByStatus = ref(true);
+
+const trainData = ref<TrainInfoObject>({});
+
+const trainTable = ref<TrainInfo[]>([]);
+const trainGroups = ref<Record<string, string[]>>({
+  arr: [],
+  dep: [],
+  sch: [],
+});
+const trainMap = ref<Array<keyof TrainInfoObject>>([]);
+const currentTrain = ref('');
 
 const refreshData = async () => {
   const response = await fetch('/trains');
   if (response.ok) trainData.value = await response.json();
 };
 
-const trainArray = computed(() => {
-  let arrived: TrainInfo[] = [];
-  let departed: TrainInfo[] = [];
-  let scheduled: TrainInfo[] = [];
-  for (const tnum in trainData.value) {
-    if (trainData.value[tnum].departed) {
-      if (trainData.value[tnum].arrived) {
-        arrived.push({number: tnum, status: 'arr', ...trainData.value[tnum]});
-      } else {
-        departed.push({number: tnum, status: 'dep', ...trainData.value[tnum]});
-      }
+const updateMarker = (trainNumber: string, coords: [number, number]) => {
+  if (leafMap.value) {
+    if (markers.value[trainNumber]) {
+      markers.value[trainNumber].setLatLng(coords);
     } else {
-      scheduled.push({number: tnum, status: 'sch', ...trainData.value[tnum]});
+      markers.value[trainNumber] = L.marker(coords)
+      .addTo(leafMap.value)
+      .bindPopup(trainNumber);
     }
   }
-  return groupByStatus.value ? [departed, scheduled, arrived] : [[...departed, ...scheduled, ...arrived]];
+};
+
+watch(trainData, (newData) => {
+  trainGroups.value = { arr: [], dep: [], sch: [] };
+  trainMap.value = [];
+  for (const tnum in newData) {
+    if (newData[tnum].departed) {
+      if (newData[tnum].arrived) {
+        trainGroups.value['arr'].push(tnum);
+        // clean up markers
+        if (markers.value[tnum]) delete markers.value[tnum];
+      } else {
+        let lat = newData[tnum].lat ?? 0;
+        let lng = newData[tnum].lng ?? 0;
+        if (newData[tnum].lat && newData[tnum].lng) {
+          // let nextStop = newData[tnum].times.findIndex((station) => station.eta !== 'ARR');
+          // if (nextStop > -1) newData[tnum].next = nextStop;
+          trainMap.value.push(tnum);
+          updateMarker(tnum, [lat, lng]);
+          mapBounds.value = [
+            [Math.max(mapBounds.value[0][0], lat), Math.min(mapBounds.value[0][1], lng)],
+            [Math.min(mapBounds.value[0][0], lat), Math.max(mapBounds.value[0][1], lng)]
+          ];
+        }
+        
+        trainGroups.value['dep'].push(tnum);
+      }
+    } else {
+      trainGroups.value['sch'].push(tnum);
+      // clean up markers
+      if (markers.value[tnum]) delete markers.value[tnum];
+    }
+  }
 });
+
+const getNextStop = (trainNumber: string) => {
+  let stopIndex = trainData.value[trainNumber].times.findIndex((station) => station.eta !== 'ARR');
+  if (stopIndex > -1) {
+    let nextStation = trainData.value[trainNumber].times[stopIndex];
+    return `${nextStation.station} (${nextStation.eta})`;
+  }
+};
+
+const selectTrain = (trainNumber: string) => {
+  currentTrain.value = trainNumber;
+  if (trainMap.value.includes(trainNumber)) {
+    leafMap.value?.flyTo([trainData.value[trainNumber].lat ?? 0, trainData.value[trainNumber].lng ?? 0], 10);
+  }
+};
 
 onMounted(async () => {
   await refreshData();
-  if (trainData.value) initialized.value = true;
+  if (trainData.value && mapElem.value) {
+    leafMap.value = L.map(mapElem.value).fitBounds(L.latLngBounds(mapBounds.value).pad(0.2));
+    //.setView(center.value, zoom.value);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(leafMap.value);
+
+    for (const train of trainMap.value) {
+      updateMarker(train as string, [trainData.value[train].lat ?? 0, trainData.value[train].lng ?? 0]);
+    }
+
+    initialized.value = true;
+  }
   setInterval(refreshData, 60 * 1000);
 });
 </script>
 
 <template>
   <main>
-    <ol-map
-      v-if="initialized"
-      :loadTilesWhileAnimating="true"
-      :loadTilesWhileInteracting="true"
-      style="height: 280px; width: 280px;"
-    >
-      <ol-view
-        ref="view"
-        :center="initCoords"
-        :rotation="0"
-        :zoom="initZoom"
-        :projection="'EPSG:4326'"
-        @change:center="centerChanged"
-        @change:resolution="resolutionChanged"
-        @change:rotation="rotationChanged"
-
-      />
-      <ol-tile-layer>
-        <ol-source-osm />
-      </ol-tile-layer>
-      <ol-overlay
-        v-for="activeTrain in trainArray[0]"
-        :position="`[${activeTrain.lng ?? 0}, ${activeTrain.lat ?? 0}]`"
-        :key="activeTrain.number"
-      >
-        <div>CHOOCHOO</div>
-      </ol-overlay>
-    </ol-map>
-    <input type="checkbox" v-model="groupByStatus">
-    <table>
+    <div id="mapElem" ref="mapElem"></div>
+    <table v-if="trainData">
       <thead>
-        <th>Train #</th>
-        <th>From</th>
-        <th>To</th>
-        <th>Status</th>
+        <tr>VIA Rail Trains</tr>
       </thead>
-      <tbody v-for="trainSet in trainArray">
-        <tr v-for="train in trainSet">
-          <td>{{ train.number }}</td>
-          <td>{{ train.from }}</td>
-          <td>{{ train.to }}</td>
-          <td>{{ train.status }}</td>
+      <tbody>
+        <tr>
+          <th>In Transit</th>
+        </tr>
+        <tr>
+          <th>Train #</th>
+          <th>From</th>
+          <th>Next Stop</th>
+          <th>Destination</th>
+          <th>Delay</th>
+        </tr>
+        <tr v-for="train in trainGroups['dep']" @click="selectTrain(train)">
+          <td>{{ train }}</td>
+          <td>{{ trainData[train].from }}</td>
+          <td>{{ getNextStop(train) }}</td>
+          <td>{{ trainData[train].to }}</td>
+          <td>{{ trainData[train].pollMin }}</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <th>Scheduled to Depart</th>
+        </tr>
+        <tr>
+          <th>Train #</th>
+          <th>From</th>
+          <th>Destination</th>
+        </tr>
+        <tr v-for="train in trainGroups['sch']" @click="selectTrain(train)">
+          <td>{{ train }}</td>
+          <td>{{ trainData[train].from }}</td>
+          <td>{{ trainData[train].to }}</td>
+        </tr>
+      </tbody>
+      <tbody>
+        <tr>
+          <th>Arrived</th>
+        </tr>
+        <tr>
+          <th>Train #</th>
+          <th>From</th>
+          <th>Destination</th>
+        </tr>
+        <tr v-for="train in trainGroups['arr']" @click="selectTrain(train)">
+          <td>{{ train }}</td>
+          <td>{{ trainData[train].from }}</td>
+          <td>{{ trainData[train].to }}</td>
         </tr>
       </tbody>
     </table>
   </main>
 </template>
+
+<style>
+#mapElem {
+  height: 500px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import './assets/base.css';
+import 'leaflet/dist/leaflet.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
-import OpenLayersMap from 'vue3-openlayers';
-
 const app = createApp(App);
-app.use(OpenLayersMap);
 
 app.mount('#app');
